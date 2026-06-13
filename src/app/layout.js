d17996c88ab510ed344a3f4f256daf61fa040513
IndranilGhoshal <indranillghoshal@gmail.com>
2026-06-13 16:05:06 +0530

    update commit

--- src/app/layout.js
+++ src/app/layout.js
@@ -179,6 +179,25 @@ export default function RootLayout({ children }) {
           </LoaderProvider>
         </NoInternet>
 
+        {process.env.NEXT_PUBLIC_GA_ID && (
+          <>
+            <Script
+              src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+              strategy="afterInteractive"
+            />
+
+            <Script id="google-analytics" strategy="afterInteractive">
+              {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}');
+        `}
+            </Script>
+          </>
+        )}
+
         <Script
           src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"
           strategy="afterInteractive"
