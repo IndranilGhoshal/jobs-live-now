8da5aa7a184484d61c13a929f8b56da61322541b
IndranilGhoshal <indranillghoshal@gmail.com>
2026-06-15 01:44:35 +0530

    update commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,6 +2,26 @@
 const nextConfig = {
   /* config options here */
   reactCompiler: true,
+  async rewrites() {
+    return [
+      {
+        source: "/sitemap.xml",
+        destination: "/sitemap",
+      },
+      {
+        source: '/sitemap-pages.xml',
+        destination: '/sitemap-pages',
+      },
+      {
+        source: '/sitemap-jobs.xml',
+        destination: '/sitemap-jobs',
+      },
+      {
+        source: '/sitemap-tools.xml',
+        destination: '/sitemap-tools',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
