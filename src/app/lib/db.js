2f6a874866073f8a97e5342d354b3b34c5a761b0
IndranilGhoshal <indranillghoshal@gmail.com>
2026-06-14 14:04:02 +0530

    update commit

--- src/app/lib/db.js
+++ src/app/lib/db.js
@@ -1,3 +1,1 @@
-import { MONGO_DB, MONGO_PASSWORD, MONGO_USERNAME } from "./config";
-
-export const connectionStr = "mongodb+srv://"+MONGO_USERNAME+":"+MONGO_PASSWORD+"@jobcluster.tu8yulz.mongodb.net/"+MONGO_DB+"?retryWrites=true&w=majority&appName=JobCluster";
+export const connectionStr = process.env.MONGO_URI;
