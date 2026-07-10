b377483332c4f1cc54b848f16ac627d8d7c847ae
IndranilGhoshal <indranillghoshal@gmail.com>
2026-07-10 22:52:40 +0530

    update commit

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,14 +1,46 @@
 export default function robots() {
-    const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
+  const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
-    return {
-        rules: [
-            {
-                userAgent: "*",
-                allow: "/",
-                disallow: ["/admin-x9k2m-secure", "/api"],
-            },
-        ],
-        sitemap: `${baseUrl}/sitemap.xml`,
-    };
+  return {
+    rules: [
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        allow: "/",
+      },
+      {
+        userAgent: "PerplexityBot",
+        allow: "/",
+      },
+      {
+        userAgent: "Googlebot",
+        allow: "/",
+      },
+      {
+        userAgent: "Google-Extended",
+        allow: "/",
+      },
+      {
+        userAgent: "Bingbot",
+        allow: "/",
+      },
+      {
+        userAgent: "Applebot",
+        allow: "/",
+      },
+      {
+        userAgent: "*",
+        allow: "/",
+        disallow: ["/admin-x9k2m-secure", "/api"],
+      },
+    ],
+    sitemap: `${baseUrl}/sitemap.xml`,
+  };
 }
